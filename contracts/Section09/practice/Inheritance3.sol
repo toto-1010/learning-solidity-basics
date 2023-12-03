e65cead4f1f1b819b2e8d2b2bf5d277f231fb042
とと <104406108+toto-1010@users.noreply.github.com>
2023-12-04 06:59:52 +0900

    Update Inheritance3.sol
    誤字修正

--- contracts/Section09/practice/Inheritance3.sol
+++ contracts/Section09/practice/Inheritance3.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.17;
 
 /** 
  * @title Inheritance(継承)を学ぼう
- * 継承したFunctionやFunctino Modifierは再定義(オーバーライド)できる
+ * 継承したFunctionやFunction Modifierは再定義(オーバーライド)できる
  *   継承元で`virtual`を使ってファンクションなどの再定義を許可し、継承先で`override`を使ってオーバーライドできる
  * Solidityは多重継承をサポートしている
  * 継承元(親)のファンクションを実行する場合は、super.を使って内部的に呼び出せる。例：super.f()
